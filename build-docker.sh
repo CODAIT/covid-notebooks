d5da4840b3300341a2ce97ca5b164637fceb9b9d
frreiss <frreiss@us.ibm.com>
2021-01-15 14:31:43 -0800

    Make Dockerfile read packages from requirements.txt

--- build-docker.sh
+++ build-docker.sh
@@ -1,2 +1,7 @@
+
+# Shell script to build the main container image for running the notebooks in
+# this project on Kubeflow Pipelines.
+cp requirements.txt etc/docker
 docker build -t codait/covid-notebooks-anaconda-py3:latest etc/docker --progress plain
+rm etc/docker/requirements.txt
 docker push codait/covid-notebooks-anaconda-py3:latest
